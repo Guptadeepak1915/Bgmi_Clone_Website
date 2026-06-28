b5c1db7cef3b2fc4be0ec439018f8a16e78ae299
guptaDeepak <deepak6br@gmail.com>
2026-06-28 21:04:39 +0530

    adding css to news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import "./News.css";
 
 const News = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchNews();
+  }, []);
+  async function fetchNews() {
+    const response = await fetch(
+      "https://api.currentsapi.services/v1/latest-news?apiKey=5Lv9L6dRcIJVFhwoArjMvNnyYf1xev1ZrJzMX6fhJIhXxiRw",
+    );
+    const data = await response.json();
+    console.log(data);
+    setNews(data.news);
+    setLoading(false);
+  }
+  if (loading) {
+    return (
+      <div class="loader-container">
+        <div class="loader"></div>
+      </div>
+    );
+  }
   return (
-    <div>
-    {/* <h1>Chala ja Bsdk <br/> Koi News Nahi Aaya hai abhi</h1> */}
-    </div>
-  )
-}
+    <div className="news-container">
+      {news.map((item, index) => (
+        <div className="card" key={index}>
+          <img src={item.image} alt={item.title} />
 
-export default News
+          <h2>{item.title}</h2>
+
+          <p>{item.description}</p>
+
+          <a href={item.url} target="_blank" rel="noopener noreferrer">
+            Read More
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default News;
